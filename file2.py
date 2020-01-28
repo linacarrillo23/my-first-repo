2030fe22e9c6b761e0372eff9120d3bd206d7cef
Lina <lina@MacBook-Pro-de-Lina.local>
2020-01-28 14:50:12 +0100

    change function and make it substract

--- file2.py
+++ file2.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jan 23 14:25:50 2020
+Created on Thu Jan 23 15:11:45 2020
 
 @author: lina
 """
 
 def func(a,b):
-    return(a - b)
+    return(a,b)
